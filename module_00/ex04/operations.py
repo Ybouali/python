0927f0a73d7dd7b08203ae7f9808a9114d483940
ybouali <yassine.bouali.bo@gmail.com>
2023-12-20 11:11:03 +0100

    done working on ex04

--- module_00/ex04/operations.py
+++ module_00/ex04/operations.py
@@ -1,7 +1,16 @@
 from curses.ascii import isdigit
 import sys
 
+
 class Operation:
+    """
+        Operation Class will do the operations
+        And prepare the result as a string
+
+        Attributes:
+            number1 (int): the first number for the operation
+            number2 (int): the secound number for the operation
+    """
     def __init__(self, number1: int, number2: int):
         self.number1 = number1
         self.number2 = number2
@@ -12,47 +21,56 @@ class Operation:
         self.__Remainder = 0
 
     def __operation(self):
-        
         """
-            operation function will return the result of the operation that is provided in the argument
+            Operation method will store the result of the operations
         """
 
+        # Product operation
         self.__Product = self.number1 * self.number2
-        
-        
+
+        # Difference operation
         self.__Difference = self.number1 - self.number2
-        
+
+        # Sum operation
         self.__Sum = self.number1 + self.number2
 
+        # Quotient operation if the secound number is not a zero number
         if self.number2 != 0:
             self.__Quotient = self.number1 / self.number2
-        
+
+        # Remainder operation if the secound number is not a zero number
         if self.number2 != 0:
             self.__Remainder = self.number1 % self.number2
-    
+
     def __str__(self):
 
+        # Call the operation method
         self.__operation()
 
+        # error message for div
         errorDivision = "ERROR (division by zero)"
 
+        # error message for Qoutient
         errorQoutient = "ERROR (modulo by zero)"
 
+        # preaper the string for the Qoutient and Remainder
+        otherString = f"Quotient:      {self.__Quotient}\n\
+            Remainder:     {self.__Remainder}"
 
-        otherString = f"Quotient:      {self.__Quotient}\nRemainder:     {self.__Remainder}"
-
-
+        # if the number2 is zero so we should set the error massage
         if self.number2 == 0:
-            otherString = f"Quotient:      {errorDivision}\nRemainder:     {errorQoutient}"
-            
+            otherString = f"Quotient:      {errorDivision}\n\
+                Remainder:     {errorQoutient}"
 
-        return f"Sum:           {self.__Sum} \nDifference:    {self.__Difference}\nProduct:       {self.__Product}\n{otherString}" 
+        # return of the string result
+        return f"Sum:           {self.__Sum} \n\
+            Difference:    {self.__Difference}\n\
+            Product:       {self.__Product}\n{otherString}"
 
 
 try:
-    
     if len(sys.argv) == 1:
-    
+
         print("Usage: python operations.py <number1> <number2>")
 
         print("Example:\n\
@@ -60,22 +78,28 @@ try:
 
         exit()
 
+    # Check if there is more than tow numbers provided
     if len(sys.argv) > 3:
         raise AssertionError(f"AssertionError: too many arguments")
+
+    # Check if there is tow numbers provided
     if len(sys.argv) != 3:
         raise AssertionError(f"U must provide tow numbers\nExample:\n\
                     python operations.py 10 3")
 
+    # Check that the arguments are are digits
     if sys.argv[1].isdigit() is False or sys.argv[2].isdigit() is False:
         raise AssertionError("AssertionError: only integers")
 
     # get the first number
     firstNumber = int(sys.argv[1])
 
+    # get the secound number
     secoundNumber = int(sys.argv[2])
 
+    # init  the class Operation
     result = Operation(number1=firstNumber, number2=secoundNumber)
-    
+
     print(result)
 
 except Exception as e:
